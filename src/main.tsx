d0b3fe2fdfce44c6228cb632c384ba4388b18dfa
Jonghun Choe <chjh6107@gmail.com>
2022-11-18 19:10:46 +0900

    codegen 테스트(실패)

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import { StrictMode, Suspense, useEffect } from "react";
 import { createRoot } from "react-dom/client";
@@ -6,13 +7,20 @@ import "src/styles/globals.css";
 import theme from "./common/theme";
 import Routes from "./routes";
 
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_END_POINT,
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <CSSReset />
-      <Suspense>
-        <Routes />
-      </Suspense>
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <CSSReset />
+        <Suspense>
+          <Routes />
+        </Suspense>
+      </ChakraProvider>
+    </ApolloProvider>
   </StrictMode>
 );
--- src/pages/equipment/rocket/index.tsx
+++ src/pages/equipment/rocket/index.tsx
@@ -1,4 +1,20 @@
+import { gql, useQuery } from "@apollo/client";
+
 const EquipmentRocketIndexPage = () => {
+  const getQuery = gql`
+    query Rockets($limit: Int, $offset: Int) {
+      rockets(limit: $limit, offset: $offset) {
+        active
+        boosters
+        company
+      }
+    }
+  `;
+
+  const { data } = useQuery(getQuery);
+
+  console.log(data);
+
   return <div>EquipmentRocketIndexPage</div>;
 };
 export default EquipmentRocketIndexPage;
